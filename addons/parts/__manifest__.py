7ca5b08aecb0424401408ed61cd8e32d5f27dd9c
ghita <ghitakhaled14@gmail.com>
2025-01-15 23:32:44 +0100

    commandes

---
+++ addons/parts/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Gestion Commandes',
+    'version': '1.1',
+    'description': "",
+    'author': 'Ghita KHALED & Assia Tourabi & kaoutar Sarsari',
+    'depends': [
+        'base',
+        'web'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml'
+    ]
+}

9aa113b0d8330f2ca15aaac232d2624ff7ef84f0
ghita <ghitakhaled14@gmail.com>
2025-01-16 12:11:20 +0100

    version final

--- addons/gestion commandes/models/commande.py
+++ addons/gestion commandes/models/commande.py
@@ -1,8 +1,26 @@
-from odoo import fields, models
+from odoo import models, fields
 
 class GestionCommandes(models.Model):
-    _name = "commande_model"
-    _description = "Gestion des commandes"
+    _name = 'gestion.commandes'
+    _description = 'Gestion des Commandes'
 
-    name = fields.Char(required=True)
+    # Définition des champs
+    name = fields.Char(string='Nom de la commande', required=True)
+    date_commande = fields.Datetime(string='Date de la commande', default=fields.Datetime.now)
+    client_id = fields.Many2one('res.partner', string='Client', required=True)
     total = fields.Float(string='Total', required=True)
+    status = fields.Selection([
+        ('draft', 'Brouillon'),
+        ('confirmed', 'Confirmée'),
+        ('done', 'Livrée')
+    ], default='draft', string='Statut')
+
+    def action_create(self):
+        """Méthode qui sera appelée pour ajouter une nouvelle commande"""
+        self.create({
+            'name': 'Nouvelle commande',  # Nom par défaut
+            'date_commande': fields.Datetime.now(),
+            'client_id': self.client_id.id,  # Client par défaut
+            'total': 0.0,  # Valeur initiale pour le total
+            'status': 'draft',
+        })
